cec5f1971160cade75847e8b01db8318de02bcf9
Salla <salladiagne14@gmail.com>
2016-03-01 16:04:32 +0100

    Removed a useless class
    Since the asciiart-core library is removed, this class becomes useless

--- src/main/scala/fil/iagl/opl/failfaster/result/IterationResultToArrayConverter.scala
+++
@@ -1,7 +0,0 @@
-package fil.iagl.opl.failfaster.result
-
-class IterationResultToArrayConverter {
-
-  def convertIterationResultToArray(iterationResult: IterationResult): Array[String] = Array(iterationResult.iterationCounter, iterationResult.nbOfModifications, iterationResult.nbOfFailingTest).map(_.toString)
-
-}
