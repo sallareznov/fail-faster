3d88bfd4e7c25e637b71d59299a044e4d80b5429
Salla <salladiagne14@gmail.com>
2016-03-01 14:55:16 +0100

    Tested the ease of use of the library (view the commit before)

--- src/main/scala/fil/iagl/opl/failfaster/Main.scala
+++ src/main/scala/fil/iagl/opl/failfaster/Main.scala
@@ -1,9 +1,16 @@
 package fil.iagl.opl.failfaster
 
 import java.io.File
+import java.util
 
 import fil.iagl.opl.failfaster.constants.ConstantsHandler
-import org.apache.commons.cli.{DefaultParser, HelpFormatter, MissingOptionException}
+import org.alcibiade.asciiart.raster.{RasterContext, ExtensibleCharacterRaster, CharacterRaster}
+import org.alcibiade.asciiart.widget.{LabelWidget, TableWidget}
+import org.alcibiade.asciiart.widget.model.{ArrayTableModel, TableModelMapAdapter}
+import org.apache.commons.cli.{HelpFormatter, MissingOptionException, DefaultParser}
+import org.klaus31.aat.AsciiArtTable
+
+import scala.collection.JavaConverters._
 
 object Main {
 
@@ -15,7 +22,7 @@ object Main {
   val INPUT_TESTS_PATH_ARGUMENT = constantsHandler.getProperty("inputTestsPathArgument").get
   val HELP_ARGUMENT = constantsHandler.getProperty("helpArgument").get
 
-  def main(args: Array[String]) {
+  /*def main(args: Array[String]) {
     val optionsHandler = new OptionsHandler()
     val options = optionsHandler.handleOptions(args, constantsHandler)
     val commandLineParser = new DefaultParser()
@@ -38,6 +45,37 @@ object Main {
         val formatter = new HelpFormatter()
         formatter.printHelp("transforms a fail-safe project to a fail-fast one", options)
     }
-  }
+  }*/
+
+  /*def main(args: Array[String]): Unit = {
+    val items = new util.TreeMap[String, Double]();
+    items.put("Paris", 42.0);
+    items.put("London", 2012.0);
+    items.put("Amsterdam", 3.1415);
+
+    val converter = new IterationResultToArrayConverter()
+    val result1 = new IterationResult(0, 0, 0)
+    val result2 = new IterationResult(1, 52, 6)
+    val result3 = new IterationResult(2, 47, 4)
+    val result4 = new IterationResult(3, 24, 1)
+
+    val array = Array.ofDim[String](4, 3)
+    //array.foreach(i => array(1))
+    array(0)(0) = 0.toString
+    array(1)(0) = 1.toString
+   /* array(0) = converter.convertIterationResultToArray(result1)
+    array(1) = converter.convertIterationResultToArray(result2)
+    array(2) = converter.convertIterationResultToArray(result3)
+    array(3) = converter.convertIterationResultToArray(result4)*/
+
+
+    val tableModel = new ArrayTableModel(array)
+    val tableWidget = new TableWidget(tableModel)
+
+    val raster = new ExtensibleCharacterRaster(' ')
+    tableWidget.render(new RasterContext(raster))
+
+    println(raster)
+  }*/
 
 }
