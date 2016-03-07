a9e639d3d4359905f3f0c103da5de765f2f53fb8
Salla <salladiagne14@gmail.com>
2016-03-07 15:33:02 +0100

    Replaces try/catch block with scala.util.Try idiom
    Using util.Try is a good, idiomatic choice, as it harnesses the type system to ensure that the user is properly considering error handling.

--- src/main/scala/fil/iagl/opl/failfaster/Main.scala
+++ src/main/scala/fil/iagl/opl/failfaster/Main.scala
@@ -3,9 +3,10 @@ package fil.iagl.opl.failfaster
 import java.io.File
 import java.util.Properties
 
-import fil.iagl.opl.failfaster.constants.{ ConstantsKeys, ConstantsHandler }
+import fil.iagl.opl.failfaster.constants.{ConstantsKeys, ConstantsHandler}
 import org.apache.commons.cli._
 
+import scala.util.{Failure, Success, Try}
 import scala.util.control.NonFatal
 
 object Main {
@@ -27,21 +28,19 @@ object Main {
     val optionsHandler = new OptionsBuilder()
     val options = optionsHandler.buildOptions(args, constantsHandler)
     val commandLineParser = new DefaultParser()
-    try {
-      val commandLine = commandLineParser.parse(options, args)
+    Try(commandLineParser.parse(options, args)) match {
+      case Success(commandLine) =>
+        val sourceFiles = new File(commandLine.getOptionValue(INPUT_SOURCES_PATH_ARGUMENT))
+        val testFiles = new File(commandLine.getOptionValue(INPUT_TESTS_PATH_ARGUMENT))
 
-      val sourceFiles = new File(commandLine.getOptionValue(INPUT_SOURCES_PATH_ARGUMENT))
-      val testFiles = new File(commandLine.getOptionValue(INPUT_TESTS_PATH_ARGUMENT))
+        val projectCompiler = new ProjectCompiler()
 
-      val projectCompiler = new ProjectCompiler()
+        projectCompiler.compileSourcesFiles(sourceFiles, constantsHandler)
+        projectCompiler.compileTestsFiles(testFiles, sourceFiles, constantsHandler)
 
-      projectCompiler.compileSourcesFiles(sourceFiles, constantsHandler)
-      projectCompiler.compileTestsFiles(testFiles, sourceFiles, constantsHandler)
-
-      val unitTestsRunner = new UnitTestsRunner()
-      unitTestsRunner.runTests(new File("spooned-classes"))
-    } catch {
-      case NonFatal(_) => usage(options)
+        val unitTestsRunner = new UnitTestsRunner()
+        unitTestsRunner.runTests(new File("spooned-classes"))
+      case Failure(exception) => usage(options)
     }
   }
 
